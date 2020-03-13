a09448c2bae82fc7980e1531e537564fdcba8068
root <root@localhost.localdomain>
2020-03-13 17:11:38 +0800

    first commit

---
+++ homework-1-6/src/ValidErr.js
@@ -0,0 +1,7 @@
+class ValidErr {
+  constructor(code, msg) {
+    this.code = code
+	this.msg = msg
+  }
+}
+module.exports = ValidErr
---
+++ homework-1-6/src/app.js
@@ -0,0 +1,81 @@
+const Koa = require('koa')
+const Router = require('@koa/router')
+const ValidErr = require('./ValidErr')
+
+const app = new Koa()
+const router = new Router()
+
+router.post('/api/user', async (ctx, next) => {
+  const reqData = await parsePostData(ctx)
+  const data = parseJson(reqData)
+  
+  assertNotNull(data, 404, '请输入json格式数据')
+  assertNotNull(data.name, 404, 'name不能为空')
+  assertNotNull(data.email, 404, 'email不能为空')
+  
+  assertEqual(ctx.header.role, 'admin', 401, 'unauthorized post')
+
+  ctx.body = {
+    code: 200,
+	data: data,
+	msg: '上传成功'
+  }
+})
+
+
+var globalErrorHandler = async (ctx, next) => {
+  try {
+    await next()
+  } catch (err) {
+	if (err instanceof ValidErr) {
+	  ctx.status = err.code
+	  ctx.body = err.msg
+	  return
+	}
+    ctx.status = 500
+	ctx.body = err
+  }
+}
+
+app
+  .use(globalErrorHandler)
+  .use(router.routes())
+  .use(router.allowedMethods())
+
+app.listen(3000)
+
+function assertNotNull(val, code, msg) {
+  if (val == null || val == undefined) {
+    throw new ValidErr(code, msg)
+  }
+}
+function assertEqual(val1, val2, code, msg) {
+  if (val1 != val2) {
+    throw new ValidErr(code, msg)
+  }
+}
+
+
+function parseJson(str) {
+  try {
+    return JSON.parse(str)
+  } catch (e) {
+    return null
+  }
+}
+
+function parsePostData(ctx) {
+  return new Promise((resolve, reject) => {
+    try {
+        let postStr = ''
+		ctx.req.on('data', function(chunk) {
+		  postStr += chunk
+		})
+		ctx.req.on('end', function() {
+		  resolve(postStr)
+		})
+	} catch(e) {
+		reject(e)
+	}
+  });
+}
